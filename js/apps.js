b06d80ad51c55353d69e5a89ebe3f382fd0bfbef
Chuma-rxt <craxoti60@gmail.com>
2024-02-23 16:02:20 +0200

    updated my skills

--- js/apps.js
+++ js/apps.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 
 $('#profile_ripple').ripples({
-    resolution:512,
-    dropRadius:10
-})
+    resolution: 512,
+    dropRadius: 10
+});
 
+const bars = document.querySelectorAll('.progress_bar');
+
+bars.forEach(function(bar){
+    let percentage = bar.dataset.percentage;
+    let tooltip = bar.children[0];
+    tooltip.innerText = percentage + '%';
+    bar.style.width = percentage + '%';
+    console.log(percentage);
+})
 
 
 });
