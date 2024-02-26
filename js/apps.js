0baeb6764e61b514d7b1777bf3e87cddba14f7be
Chuma-rxt <craxoti60@gmail.com>
2024-02-26 09:45:16 +0200

    intergrating Js on the counter

--- js/apps.js
+++ js/apps.js
@@ -15,5 +15,36 @@ bars.forEach(function(bar){
     console.log(percentage);
 })
 
+// ================COUNTER==================
+
+const counters = document.querySelectorAll('.counter');
+console.log(counters);
+
+function runCounter (){
+    counters.forEach(counter => {
+        counter.innerText = 0;
+        let target = +counter.dataset.count;
+        let step = target / 100;
+
+       
+
+        let countIt = function() {
+            let displayedCount = +counter.innerText;
+            if (displayedCount < target) {
+
+                counter.innerText = Math.ceil(displayedCount + step);
+                
+                setTimeout(countIt, 1);
+            } else {
+                counter.innerText = target;
+            }
+        }
+        countIt();
+
+    })
+}
+
+runCounter();
+
 
 });
