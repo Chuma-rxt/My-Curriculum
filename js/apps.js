4cfd86753764857153df61b9b6e815393f61e9fa
Chuma-rxt <craxoti60@gmail.com>
2024-02-27 13:47:11 +0200

    intergrating my cn with the button

--- js/apps.js
+++ js/apps.js
@@ -1,171 +1,94 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // Ripples Effect
+    $('#profile_ripple').ripples({
+        resolution: 512,
+        dropRadius: 10
+    });
 
-$('#profile_ripple').ripples({
-    resolution: 512,
-    dropRadius: 10
-});
+    // Progress Bars
+    const bars = document.querySelectorAll('.progress_bar');
+    bars.forEach(function(bar) {
+        let percentage = bar.dataset.percentage;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    });
 
-const bars = document.querySelectorAll('.progress_bar');
+    // Counters
+    const counters = document.querySelectorAll('.counter');
+    function runCounter() {
+        counters.forEach(counter => {
+            counter.innerText = 0;
+            let target = +counter.dataset.count;
+            let step = target / 100;
+            let countIt = function() {
+                let displayedCount = +counter.innerText;
+                if (displayedCount < target) {
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    setTimeout(countIt, 90);
+                } else {
+                    counter.innerText = target;
+                }
+            };
+            countIt();
+        });
+    }
+    runCounter();
 
-bars.forEach(function(bar){
-    let percentage = bar.dataset.percentage;
-    let tooltip = bar.children[0];
-    tooltip.innerText = percentage + '%';
-    bar.style.width = percentage + '%';
-    console.log(percentage);
-})
-
-// ================COUNTER==================
-
-const counters = document.querySelectorAll('.counter');
-
-
-function runCounter (){
-    counters.forEach(counter => {
-        counter.innerText = 0;
-        let target = +counter.dataset.count;
-        let step = target / 100;
-
-       
-
-        let countIt = function() {
-            let displayedCount = +counter.innerText;
-            if (displayedCount < target) {
-
-                counter.innerText = Math.ceil(displayedCount + step);
-                
-                setTimeout(countIt, 90);
-            } else {
-                counter.innerText = target;
-            }
+    // Intersection Observer for Counters
+    let done = 0;
+    const sectionObserver = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && done !== 1) {
+            done = 1;
+            runCounter();
         }
-        countIt();
+    }, { rootMargin: '0px 0px -200px 0px' });
 
-    })
-}
+    // Isotope Filtering
+    var $wrapper = $('.portfolio_wrapper');
+    $wrapper.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
 
-runCounter();
-
-let counterSection = document.querySelector('.counter_section');
-
-let options = {
-    rootMargin : '0px 0px -200px 0px'
-}
-
-let done = 0;
-
-const sectionObserver = new  IntersectionObserver (function(entries){
-
-    if (entries[0].isIntersecting && done!== 1){
-        done = 1;
-        runCounter();
-    }
-
-}, options)
-
-// sectionObserver.observe(counterSection);
-
-
-// //===========Image===========
-
-//  var $wrapper = $('portfolio_wrapper');
-
-// //=======Initialize isotope
-
-//  $wrapper.isotope({
-//      filter : '*',
-//      layoutMode : 'masonry',
-//      animationOptions : {
-//          duration: 750,
-//          easing: 'linear'
-//     }
-
-//  });
-
-//  let links = document.querySelectorAll('.tabs a');
-
-//  links.forEach(link =>{
-
-//      let selector = link.dataset.filter;
-    
-//      link.addEventListener('click', function(e){
-//          e.preventDefault();
-
-//          $wrapper.isotope({
-//              filter : selector,
-//              layoutMode : 'masonry',
-//              animationOptions : {
-//                 duration: 750,
-//                 easing: 'linear'
-//             }
-        
-//         });
-        
-//     })
-//  })
-// })
-
-var $wrapper = $('.portfolio_wrapper'); // Use '$' to indicate it's a jQuery object
-
-//=======Initialize isotope
-$wrapper.isotope({
-    filter: '*',
-    layoutMode: 'masonry',
-    animationOptions: {
-        duration: 750,
-        easing: 'linear'
-    }
-});
-
-let links = document.querySelectorAll('.tabs a');
-
-links.forEach(link => {
-    let selector = link.dataset.filter;
-
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        $wrapper.isotope({
-            filter: selector,
-            layoutMode: 'masonry',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear'
+    let links = document.querySelectorAll('.tabs a');
+    links.forEach(link => {
+        let selector = link.dataset.filter;
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            $wrapper.isotope({
+                filter: selector,
+                layoutMode: 'masonry',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear'
                 }
             });
-
             links.forEach(link => {
                 link.classList.remove('active')
-            })
-
+            });
             e.target.classList.add('active');
         });
     });
 
-
-
-    //=========Magnify pop up
-    $('.magnify').magnificPopup ({
+    // Magnify Popup for Images
+    $('.magnify').magnificPopup({
         type: 'image',
         gallery: {
-            enabled : true
+            enabled: true
         },
-        zoom : {
+        zoom: {
             enable: true
         }
     });
 
-    // =============Slider=========
-    // $('.slider').slick({
-    //     arrows: false,
-    //     autoplay: true
-    //   });
-
- 
-
-      
+    // Slick Slider
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true
+    });
 });
-
-
-
-
